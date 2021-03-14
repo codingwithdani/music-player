5ec620a31e4ab9fb71c18503a2be418977fec3fa
Daniela <aguileramalave@gmail.com>
2021-03-14 23:56:19 +0100

    feat: add content layout component

--- src/components/ContentLayout/index.jsx
+++ src/components/ContentLayout/index.jsx
@@ -2,8 +2,8 @@
 import { Header } from 'components/Header'
 import { ContentLayoutStyles } from './styles'
 
-export const ContentLayout = ({ children }) => 
-    <ContentLayoutStyles>
-        <Header />
-        {children}
-    </ContentLayoutStyles>
+export const ContentLayout = ({ children }) =>
+  <ContentLayoutStyles>
+    <Header />
+    {children}
+  </ContentLayoutStyles>
---
+++ src/components/ContentLayout/styles.js
@@ -0,0 +1,12 @@
+
+import styled from 'styled-components'
+
+export const ContentLayoutStyles = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #252729;
+    max-width: 800px;
+    min-height: 400px; 
+    border-radius: 0 0 5px 5px;
+    width: 100%;
+`
