c90fa35711a2b41b3aa0d06f8721488547c82030
Daniela <aguileramalave@gmail.com>
2021-03-14 23:20:57 +0100

    feat: add layout styles

---
+++ src/styles-utils/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    min-height: 100vh;
+    background-color: #1a1c1e;
+
+`
