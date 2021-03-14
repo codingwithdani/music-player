8452ec4c9271e0e42d087f3fb4855990658061d5
Daniela <aguileramalave@gmail.com>
2021-03-14 23:51:23 +0100

    feat: add store and provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {store} from './store'
-import {Provider} from 'react-redux'
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,9 +13,9 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
