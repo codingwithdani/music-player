38eb0fdf1e6a0567ab996e13515ac70abbdf96c8
Daniela <aguileramalave@gmail.com>
2021-03-14 23:54:03 +0100

    feat: add error 404 component

---
+++ src/pages/Error404/index.jsx
@@ -0,0 +1,8 @@
+import { Gif, Text } from './styles'
+
+export const Error404 = () => (
+  <div>
+    <Text>Opps! Page not found</Text>
+    <Gif src='giphy.gif' />
+  </div>
+)
---
+++ src/pages/Error404/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Gif = styled.img`
+    width: 100%;
+    border-radius: 0 0 5px 5px;
+`
+export const Text = styled.span`
+    font-size: 30px;
+    color: gray;
+    padding-left: 20px;
+`
