cf4f8e91e529ee29b738a62b40473d1cc02388c9
Daniela <aguileramalave@gmail.com>
2021-03-14 23:54:44 +0100

    feat: add single track details component

---
+++ src/components/SingleTrackDetails/index.jsx
@@ -0,0 +1,46 @@
+import { SingleTrackDetailsContainer, TrackImage, TrackName, TrackSinger } from './styles'
+import { Player } from 'components/Player'
+import { useSelector } from 'react-redux'
+
+export const SingleTrackDetails = ({ trackId }) => {
+  const { musicList } = useSelector(state => state.music)
+
+  const trackIndex = musicList.findIndex(song => song.trackId === Number(trackId))
+  console.log({ trackIndex })
+
+  const getInfoFromMusicList = () => {
+    return trackIndex >= 0
+      ? musicList[trackIndex]
+      : {}
+  }
+
+  const prevTrackId = trackIndex >= 1
+    ? musicList[trackIndex - 1].trackId
+    : undefined
+
+  const nextTrackId = trackIndex >= 0 && trackIndex < musicList.length - 1
+    ? musicList[trackIndex + 1].trackId
+    : undefined
+
+  const {
+    artistName,
+    artworkUrl100,
+    previewUrl,
+    trackName
+  } = getInfoFromMusicList(trackIndex)
+
+  return (
+    <SingleTrackDetailsContainer>
+      {trackIndex >= 0
+        ? (
+          <>
+            <TrackImage src={artworkUrl100} alt={trackName} />
+            <TrackName>{trackName}</TrackName>
+            <TrackSinger>{artistName}</TrackSinger>
+            <Player nextTrackId={nextTrackId} prevTrackId={prevTrackId} trackId={trackId} previewUrl={previewUrl} />
+          </>
+          )
+        : <p style={{ color: 'white' }}>Este track no se encuentra disponible</p>}
+    </SingleTrackDetailsContainer>
+  )
+}
---
+++ src/components/SingleTrackDetails/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const SingleTrackDetailsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    padding-top: 30px;
+`
+
+export const TrackImage = styled.img`
+    width: 200px;
+    height: 250px;
+    border-radius: 4px;
+    border: 1px solid #ffff;
+`
+
+export const TrackName = styled.h1`
+    color: #ffffff;
+    margin: 0;
+    font-size: 20px;
+    font-weight: 500;
+`
+
+export const TrackSinger = styled.h2`
+    color: #ffffff;
+    margin: 0;
+    font-size: 20px;
+    font-weight: 200;
+`
