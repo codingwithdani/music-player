3aa15b570fa2f9a77f20f4d6b861f9b5b6381a1c
Daniela <aguileramalave@gmail.com>
2021-03-15 17:29:40 +0100

    feat: add search global state

--- src/pages/SearchSongs/index.jsx
+++ src/pages/SearchSongs/index.jsx
@@ -3,16 +3,26 @@ import { EmptySearch } from 'components/EmptySearch'
 import { Table } from 'components/Table'
 import { makeData, columns } from 'components/Table/data'
 import { useSelector } from 'react-redux'
+import { SearchSongContainer, QueryText } from './styles'
 
 export const SearchSongs = () => {
-  const { musicList } = useSelector(state => state.music)
+  const { musicList, searchTerm } = useSelector(state => {
+    const { music, search } = state
+    const { musicList } = music
+    const { searchTerm } = search
+    return { musicList, searchTerm }
+  })
+
   return (
-    <>
+    <SearchSongContainer>
+      {searchTerm && musicList.length
+        ? <QueryText>{`Searching "${searchTerm}"`}</QueryText>
+        : <div />}
       {
         musicList && musicList.length
           ? <Table data={makeData(musicList)} columns={columns} />
           : <EmptySearch />
       }
-    </>
+    </SearchSongContainer>
   )
 }
--- src/state/index.js
+++ src/state/index.js
@@ -1,1 +1,2 @@
 export { default as musicReducers } from './music'
+export { default as searchReducers } from './search'
