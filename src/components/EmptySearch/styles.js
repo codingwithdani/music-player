ab206d8ed94c3d1d58ecabddf892c1f51d584b52
Daniela <aguileramalave@gmail.com>
2021-03-15 17:27:43 +0100

    fix: fix height in emptysearch container

--- src/components/EmptySearch/styles.js
+++ src/components/EmptySearch/styles.js
@@ -2,8 +2,9 @@ import styled from 'styled-components'
 
 export const EmptySearchContainer = styled.div`
     display: flex;
-    height: 100%;
-    flex: 1;
+    min-height: 400px;
+     
+  
     justify-content: center;
     align-items: center;
     flex-direction: column;
