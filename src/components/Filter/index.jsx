4a53fca6c236e35e522fcccc8bd66e8d52967f40
Daniela <aguileramalave@gmail.com>
2021-03-16 22:22:57 +0100

    feat: add redirect on filter

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -2,21 +2,25 @@ import debounce from 'just-debounce-it'
 import { useDispatch } from 'react-redux'
 import { fetchMusicList } from 'state/music'
 import { setSearchTerm } from 'state/search'
+import { useLocation } from 'wouter'
 import { FilterContainer, InputStyled, Icon } from './styles'
 
 export const Filter = () => {
   const dispatch = useDispatch()
+  const [location, setLocation] = useLocation()
 
   const debounceDispatch = debounce(searchTerm => dispatch(fetchMusicList(searchTerm)), 500)
 
   const handleChange = e => {
+    if (location !== '/') setLocation('/')
+
     debounceDispatch(e.target.value)
     dispatch(setSearchTerm(e.target.value))
   }
 
   return (
     <FilterContainer>
-      <Icon src='magnifying-glass_search-bar.svg' />
+      <Icon alt='Magnify glass' src='/magnifying-glass_search-bar.svg' />
       <InputStyled data-testid='input-search' onChange={handleChange} />
     </FilterContainer>
   )
