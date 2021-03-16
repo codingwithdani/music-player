7d825fc4de94170cb85ee43e4e36783d096a889e
Daniela <aguileramalave@gmail.com>
2021-03-16 22:23:12 +0100

    feat: add link and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Filter } from 'components/Filter'
 import { Link } from 'wouter'
-import { HeaderContainer, HeaderText } from './styles'
+import { HeaderContainer, HeaderLink, HeaderText } from './styles'
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <Link href='/'><HeaderText>CornerJob Music Player</HeaderText></Link>
+      <Link href='/'>
+        <HeaderLink>
+          <HeaderText>CornerJob Music Player</HeaderText>
+        </HeaderLink>
+      </Link>
       <Filter />
     </HeaderContainer>
   )
